0a6aa9ae365bff05c93b14c2f81b033d5720841b
rongon <rongonhasan2@gmail.com>
2022-10-27 10:35:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+const coursesCollection = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("Now server is running");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(coursesCollection);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const item = coursesCollection?.find((p) => p.id == id);
+  if (!item) {
+    res.send("khuje pai nai");
+  }
+  res.send(item);
+});
+
+app.listen(Port, () => {
+    console.log("server is running", Port);
+  });
+  
+  module.exports = app;
